6a909dbdfdf973645a1a5dead8ad717e72f3faf6
Nick Nance <nance.nick@gmail.com>
2022-11-24 17:32:32 -0600

    dev server with echo route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+import { createServer, IncomingMessage, ServerResponse } from 'http'
+import { parse } from 'url'
+
+const port = 8080
+const host = 'localhost'
+
+const serverHandler = (req: IncomingMessage): string | string[] => {
+    if (req.url) {
+        const parsedURL = parse(req.url, true)
+        if (parsedURL.pathname == '/echo' && parsedURL.query.message) {
+            return parsedURL.query.message
+        }
+    }
+    return 'Page not found'
+}
+
+const server = createServer((req, res) => {
+    res.end(serverHandler(req))
+})
+
+server.listen(port, host, function () {
+    console.log(`Web server is running on port ${port}`)
+})
